b48d92cd179036c114e102585d6b566dab68502e
Fausto Jose Tineo Mejia <2016-2155@est.itsc.edu.do>
2021-08-08 20:46:56 -0400

    Agregar archivos de proyecto.

---
+++ Ejercicio 11 Ultima Letra/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ejercicio_11_Ultima_Letra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("La ultima letra de esta palabra es :" + Cadena2("Hola"));
+        }
+        public static string Cadena2(string palabra)
+        {
+            string ultima = palabra.Substring(palabra.Length-1,1);
+            return ultima;
+        }
+    }
+}
